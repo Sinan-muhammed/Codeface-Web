1902331749b38acdf6598df9d9a9999873443edc
Sinan-muhammed <sinasinan418@gmail.com>
2025-03-18 03:02:26 +0530

    Fix styling issue on homepage

--- scripts/counter.js
+++ scripts/counter.js
@@ -59,3 +59,15 @@
       (char, i) => `<span style="transform:rotate(${i * 8}deg)">${char}</span>`
     )
     .join("");
+
+
+
+    const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for(let i=0; i<marqueeElementsDisplayed; i++) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
