80f17ee93678370789b16adf1b21d6ba9bbaf191
Sinan-muhammed <sinasinan418@gmail.com>
2025-03-18 10:07:52 +0530

    Fix styling issue on homepage

--- scripts/nav.js
+++ scripts/nav.js
@@ -3,3 +3,19 @@
     const navbar = document.querySelector('.navbar');
     navbar.classList.toggle('active');
   }
+
+
+
+
+  
+
+  function updateButtonVisibility() {
+    const scrollLeft = caseStudiesContainer.scrollLeft;
+    const maxScroll = caseStudiesContainer.scrollWidth - caseStudiesContainer.clientWidth;
+  
+    scrollLeftButton.style.visibility = scrollLeft > 0 ? 'visible' : 'hidden';
+    scrollRightButton.style.visibility = scrollLeft < maxScroll ? 'visible' : 'hidden';
+  }
+  
+  caseStudiesContainer.addEventListener('scroll', updateButtonVisibility);
+  updateButtonVisibility(); // Initial check
